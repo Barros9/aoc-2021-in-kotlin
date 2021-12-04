e9ba1aacdd88e0f8e52db35ac729928a529664b6
Barros9 <giu.barreca@gmail.com>
2021-12-04 10:12:03 +0100

    day04

---
+++ src/day04/Day04.kt
@@ -0,0 +1,88 @@
+package day04
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val draws = input.first().split(",").map { it.toInt() }
+        val cards = input.drop(2).windowed(size = 5, step = 6).map { parseCard(it) }
+
+        draws.forEach { draw ->
+            cards.forEach { card ->
+                card.mark(draw)
+                if (card.isBingo()) {
+                    return draw * card.sumUnmarked()
+                }
+            }
+        }
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        val draws = input.first().split(",").map { it.toInt() }
+        val cards = input.drop(2).windowed(size = 5, step = 6).map { parseCard(it) }
+        lateinit var lastWinnerCard: Pair<Int, Card>
+
+        draws.forEach { draw ->
+            cards.forEach { card ->
+                if (!card.isAWinnerCard) {
+                    card.mark(draw)
+                    if (card.isBingo()) {
+                        lastWinnerCard = Pair(draw, card)
+                    }
+                }
+            }
+        }
+        return lastWinnerCard.first * lastWinnerCard.second.sumUnmarked()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day04/Day04_test")
+    check(part1(testInput) == 4512)
+    check(part2(testInput) == 1924)
+
+    val input = readInput("day04/Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun parseCard(input: List<String>) =
+    Card(input.flatMap { row -> row.trim().replace("\\s+".toRegex(), ",").split(",").map { Box(it.toInt()) } })
+
+data class Box(val number: Int, var isMarked: Boolean = false)
+
+class Card(private val boxes: List<Box>) {
+    var isAWinnerCard = false
+
+    fun mark(number: Int) {
+        boxes.forEach { if (it.number == number) it.isMarked = true }
+    }
+
+    fun isBingo(): Boolean {
+        for (i in 0 until 5) {
+            if (rowBingo(i) || columnBingo(i)) {
+                isAWinnerCard = true
+                return true
+            }
+        }
+        return false
+    }
+
+    fun sumUnmarked() = boxes.filter { !it.isMarked }.sumOf { it.number }
+
+    private fun rowBingo(index: Int) = listOf(
+        boxes[index * 5 + 0],
+        boxes[index * 5 + 1],
+        boxes[index * 5 + 2],
+        boxes[index * 5 + 3],
+        boxes[index * 5 + 4]
+    ).all { it.isMarked }
+
+    private fun columnBingo(index: Int) = listOf(
+        boxes[index + 5 * 0],
+        boxes[index + 5 * 1],
+        boxes[index + 5 * 2],
+        boxes[index + 5 * 3],
+        boxes[index + 5 * 4]
+    ).all { it.isMarked }
+}
