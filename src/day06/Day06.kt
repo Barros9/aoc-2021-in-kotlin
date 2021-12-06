eb4795730b00d00735d371cd0f6c45e0aae5e438
Barros9 <giu.barreca@gmail.com>
2021-12-06 09:45:02 +0100

    day06

---
+++ src/day06/Day06.kt
@@ -0,0 +1,44 @@
+package day06
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        var fishes = parseFishes(input)
+        repeat(80) { fishes = fishes.nextDay() }
+        return fishes.values.sum()
+    }
+
+    fun part2(input: List<String>): Long {
+        var fishes = parseFishes(input)
+        repeat(256) { fishes = fishes.nextDay() }
+        return fishes.values.sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day06/Day06_test")
+    check(part1(testInput) == 5934L)
+    check(part2(testInput) == 26984457539L)
+
+    val input = readInput("day06/Day06")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun parseFishes(input: List<String>): MutableMap<Int, Long> =
+    input
+        .first()
+        .split(',')
+        .map { it.toInt() }
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { (key, value) -> value.toLong() }
+        .toMutableMap()
+
+fun MutableMap<Int, Long>.nextDay(): MutableMap<Int, Long> =
+    mapKeysTo(mutableMapOf()) { (key) -> key - 1 }.apply {
+        this.remove(-1)?.also {
+            this[6] = it + (this[6] ?: 0L)
+            this[8] = it
+        }
+    }
